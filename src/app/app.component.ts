981a37f1972e380e53ea357bd2f0e66d2874032f
artem likhtorovych <artem.likhtorovich89@gmail.com>
2020-03-31 02:07:29 +0300

    add custom element to palette and context pad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import Modeler from 'bpmn-js/dist/bpmn-modeler.production.min.js';
-// import BpmnPalletteModule from 'bpmn-js/lib/features/palette';
-import { cp } from 'palette/palette';
+import customControlsModule from '../custom';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  modeler: any;
+export class AppComponent implements OnInit, OnDestroy {
+  private modeler: any;
 
   constructor() {}
 
@@ -17,19 +16,12 @@ export class AppComponent implements OnInit {
     this.modeler = new Modeler({
       keyboard: { bindTo: document },
       container: '#canvas',
-      width: '100%',
       additionalModules: [
-          {}
-        // {
-        //   __init__: [ 'interactionLogger' ],
-        //   interactionLogger: [ 'type', (eventBus) => {
-        //     eventBus.on('element.hover', (event) => {
-        //       console.log('here');
-        //     });
-        //   } ]
-        // }
+        customControlsModule
       ]
     });
+    this.modeler.get('canvas').zoom('fit-viewport');
+    // this.modeler.addCustomElements(customControlsModule);
     this.modeler.createDiagram(null, (err: any) => {
       console.log(err);
     });
@@ -40,5 +32,9 @@ export class AppComponent implements OnInit {
       console.log(err, xml);
 
     });
+  }
+
+  ngOnDestroy(): void {
+    this.modeler.destroy();
   }
 }
--- src/typing.d.ts
+++ src/typing.d.ts
@@ -1,3 +1,6 @@
 declare module 'bpmn-js';
 declare module 'bpmn-js/dist/bpmn-modeler.production.min.js';
-declare module 'bpmn-js/lib/features/palette/PaletteProvider';
+declare module 'custom';
+// declare module 'bpmn-js/lib/features/context-pad/ContextPadProvider';
+// declare module 'bpmn-js/lib/features/palette/PaletteProvider';
+// declare module 'bpmn-js/lib/features/palette/PaletteProvider';
