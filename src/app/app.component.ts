1d74ff6a20b8e08ab76f72b123cc75085b7f0894
artem likhtorovych <artem.likhtorovich89@gmail.com>
2020-03-30 14:06:30 +0300

    typing add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Modeler from 'bpmn-js/dist/bpmn-modeler.production.min.js';
+// import BpmnPalletteModule from 'bpmn-js/lib/features/palette';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-bpmn-js';
+export class AppComponent implements OnInit {
+  modeler: any;
+
+  ngOnInit() {
+    this.modeler = new Modeler({
+      keyboard: { bindTo: document },
+      container: '#canvas',
+      width: '100%'
+    });
+    this.modeler.createDiagram(null, (err: any) => {
+      console.log(err);
+    });
+  }
 }
---
+++ src/typing.d.ts
@@ -0,0 +1,2 @@
+declare module 'bpmn-js';
+declare module 'bpmn-js/dist/bpmn-modeler.production.min.js';
